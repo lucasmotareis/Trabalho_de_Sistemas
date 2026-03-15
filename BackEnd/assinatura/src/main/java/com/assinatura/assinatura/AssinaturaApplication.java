1a94de379904766f98cbe4bfd880a7f0fc922dc9
Lucas Mota Reis <lucasmotareis@gmail.com>
2026-03-15 09:46:16 -0300

    chore: initial monorepo setup2

---
+++ BackEnd/assinatura/src/main/java/com/assinatura/assinatura/AssinaturaApplication.java
@@ -0,0 +1,13 @@
+package com.assinatura.assinatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssinaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssinaturaApplication.class, args);
+	}
+
+}
