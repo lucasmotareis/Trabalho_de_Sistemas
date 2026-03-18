b18b09d8696111fbe6548f9ecf2c6a0fc84248f0
Lucas Mota Reis <lucasmotareis@gmail.com>
2026-03-18 14:14:29 -0300

    add new route on CORS

--- BackEnd/assinatura/src/main/java/com/assinatura/assinatura/config/SecurityConfig.java
+++ BackEnd/assinatura/src/main/java/com/assinatura/assinatura/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://trabsistemafront.pmto8bpm.com.br"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
